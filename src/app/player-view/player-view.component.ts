3b1b512c39f0ea71d88c9d233c1fe861619509eb
anton-novak <anton-novak@outlook.com>
2023-04-19 20:07:20 +0100

    feat: implement jsmedia tags, track info, seek feature, render in-file cover art

--- src/app/player-view/player-view.component.ts
+++ src/app/player-view/player-view.component.ts
@@ -1,24 +1,32 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { DefaultUrlSerializer } from '@angular/router';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+// jsmediatags IS A LIBRARY THAT READS MEDIA TAGS
+import * as jsmediatags from 'jsmediatags';
+import { PictureType } from 'jsmediatags/types';
 
 @Component({
   selector: 'app-player-view',
   templateUrl: './player-view.component.html',
   styleUrls: ['./player-view.component.css']
 })
-export class PlayerViewComponent implements AfterViewInit {
+export class PlayerViewComponent {
   // ViewChild and ElementRef ARE AN ANGULAR WAYS TO WRAP AND REFER TO DOM ELEMENTS
-  // USING DOM SELECTORS IS NOT FOR ANGULAR
+  // USING DOM SELECTORS DIRECTLY IS NOT FOR ANGULAR
   @ViewChild("audio") audioElement!: ElementRef;
   @ViewChild("volume") volumeControl!: ElementRef;
   @ViewChild("progressBar") progressBar!: ElementRef;
+  @ViewChild("cover") coverArt!: ElementRef;
 
-  playing = false;
-  duration: String = "0:00";
-  currentTime: String = "0:00";
+  playing: boolean = false;
+  duration: string = "0:00";
+  currentTime: string = "0:00";
 
+  info = {
+    artist: "" as string,
+    title: "" as string
+  };
+
+  // REFERRING TO THE ELEMENTS IN THE HANDLERS TO MAKE SURE THEY ARE INITIALIZED AND NOT NULL
   playPause(event: Event): void {
-    // REFERRING TO THE ELEMENT IN THE HANDLER TO MAKE SURE IT IS INITIALIZED AND IS NOT NULL
     const audioElement = this.audioElement.nativeElement;
     if (this.playing === false) {
       audioElement.play();
@@ -29,7 +37,7 @@ export class PlayerViewComponent implements AfterViewInit {
     };
   };
 
-  changeVolume(event: Event): void {
+  changeVolume(): void {
     const volumeControl = this.volumeControl.nativeElement;
     const audioElement = this.audioElement.nativeElement;
     audioElement.volume = Number(volumeControl.value);
@@ -38,21 +46,46 @@ export class PlayerViewComponent implements AfterViewInit {
   getMetadata(): void {
     const audioElement = this.audioElement.nativeElement;
     const progressBar = this.progressBar.nativeElement;
-    audioElement.addEventListener("loadedmetadata", (event: Event) => {
-      const duration = audioElement.duration;
-      this.duration = this.parseTime(duration);
-      progressBar.max = duration;
+    const coverArt = this.coverArt.nativeElement;
+
+    const duration = audioElement.duration;
+    this.duration = this.parseTime(duration) as string;
+    progressBar.max = duration;
+
+    jsmediatags.read(audioElement.src, {
+      onSuccess: (result) => {
+        console.log(result);
+        this.info.artist = result.tags.artist!;
+        this.info.title = result.tags.title!;
+        // APPARENTLY SOME FILES CONTAIN IMAGE DATA AS WELL
+        // PARSING BELOW AS ADVISED BY AUTHORS OF THE LIBRARY
+        const { data, format } = result.tags.picture!;
+        let base64String = "";
+        for (let i = 0; i < data.length; i++) {
+          base64String += String.fromCharCode(data[i]);
+        }
+        coverArt.src = `data:${format};base64,${window.btoa(base64String)}`;
+      },
+      onError: (error) => {
+        console.error(error);
+      }
     });
   }
 
   updateTime(): void {
     const audioElement = this.audioElement.nativeElement;
     const progressBar = this.progressBar.nativeElement;
-    audioElement.addEventListener("timeupdate", (event: Event) => {
-      const currentTime = audioElement.currentTime;
-      this.currentTime = this.parseTime(currentTime);
-      progressBar.value = currentTime;
-    });
+    const currentTime = audioElement.currentTime;
+    this.currentTime = this.parseTime(currentTime) as string;
+    progressBar.value = currentTime;
+  }
+
+  seek(event: MouseEvent): void {
+    const audioElement = this.audioElement.nativeElement;
+    const progressBar = this.progressBar.nativeElement;
+    let newTime = event.offsetX / progressBar.offsetWidth * audioElement.duration;
+    progressBar.value = newTime;
+    audioElement.currentTime = newTime;
   }
 
   parseTime(totalSeconds: number): String {
@@ -60,11 +93,6 @@ export class PlayerViewComponent implements AfterViewInit {
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = totalSeconds % 60;
     return `${minutes}:${(seconds % 60).toString().padStart(2, "0")}`;
-  }
-
-  ngAfterViewInit(): void {
-    this.getMetadata();
-    this.updateTime();
   }
 }
 

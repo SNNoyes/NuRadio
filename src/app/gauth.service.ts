68af8c45b4731ffefca507b31f7218a3334acdc0
anton-novak <anton-novak@outlook.com>
2023-04-25 13:17:32 +0100

    feat: migrate gauth code into a service

--- src/app/gauth.service.ts
+++ src/app/gauth.service.ts
@@ -1,9 +1,129 @@
 import { Injectable } from '@angular/core';
+import { CLIENT_ID, API_KEY, SCOPES, DISCOVERY_DOC } from 'src/env';
+
+// GOOGLE-BASED CODE
+declare const google: any;
+declare const gapi: any;
+// GOOGLE-BASED CODE
 
 @Injectable({
   providedIn: 'root'
 })
 export class GauthService {
+  constructor(
 
-  constructor() { }
+  ) { 
+    this.createScript1();
+    this.createScript2();
+  }
+
+  tokenClient: any;
+  gapiInited = false;
+  gisInited = false;
+
+  gapiLoaded() {
+    gapi.load('client', this.initializeGapiClient);
+  }
+
+  async initializeGapiClient() {
+    await gapi.client.init({
+      apiKey: API_KEY,
+      // discoveryDocs: [this.DISCOVERY_DOC],
+      discoveryDocs: [DISCOVERY_DOC]
+    });
+    this.gapiInited = true;
+    // I DO NOT IMPLEMENT THIS - THE BUTTONS ARE ALWAYS VISIBLE
+    // maybeEnableButtons();
+  }
+
+  gisLoaded() {
+    this.tokenClient = google.accounts.oauth2.initTokenClient({
+      client_id: CLIENT_ID,
+      scope: SCOPES,
+      callback: '', // defined later
+    });
+    this.gisInited = true;
+    // maybeEnableButtons();
+  }
+
+  handleAuthClick() {
+    this.tokenClient.callback = async (resp: any) => {
+      if (resp.error !== undefined) {
+        throw (resp);
+      }
+      // document.getElementById('signout_button').style.visibility = 'visible';
+      // document.getElementById('authorize_button').innerText = 'Refresh';
+      // await this.listFiles();
+    };
+
+    if (gapi.client.getToken() === null) {
+      // Prompt the user to select a Google Account and ask for consent to share their data
+      // when establishing a new session.
+      this.tokenClient.requestAccessToken({prompt: 'consent'});
+    } else {
+      // Skip display of account chooser and consent dialog for an existing session.
+      this.tokenClient.requestAccessToken({prompt: ''});
+    }
+  }
+
+  handleSignoutClick() {
+    const token = gapi.client.getToken();
+    if (token !== null) {
+      google.accounts.oauth2.revoke(token.access_token);
+      gapi.client.setToken('');
+      // document.getElementById('content').innerText = '';
+      // document.getElementById('authorize_button').innerText = 'Authorize';
+      // document.getElementById('signout_button').style.visibility = 'hidden';
+    }
+  }
+
+  async listFiles() {
+    let response;
+    try {
+      response = await gapi.client.drive.files.list({
+        'pageSize': 10,
+        'fields': 'files(id, name)',
+      });
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+    const files = response.result.files;
+    if (!files || files.length == 0) {
+      // document.getElementById('content').innerText = 'No files found.';
+      console.log('No files found');
+      return;
+    }
+    // Flatten to string to display
+    const output = files.reduce(
+        (str: any, file: any) => `${str}${file.name} (${file.id})\n`,
+        'Files:\n');
+    console.log(output);
+  }
+  // GOOGLE-BASED CODE
+
+  createScript1(): void {
+    const script1 = window.document.createElement('script');
+    script1.type = 'text/javascript';
+    script1.src = 'https://apis.google.com/js/api.js';
+    script1.async = true;
+    script1.defer = true;
+    script1.onload = ((event: Event) => {
+      this.gapiLoaded();
+    })
+    window.document.head.appendChild(script1);
+  }
+  
+  createScript2(): void {
+    const script2 = window.document.createElement('script');
+    script2.type = 'text/javascript';
+    script2.src = 'https://accounts.google.com/gsi/client';
+    script2.async = true;
+    script2.defer = true;
+    script2.onload = ((event: Event) => {
+      this.gisLoaded();
+    })
+    window.document.head.appendChild(script2);
+  }
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,15 +4,13 @@ import { GoogleLoginProvider, SocialAuthService, GoogleSigninButtonDirective } f
 import { SocialUser } from '@abacritt/angularx-social-login';
 import { HttpClient } from '@angular/common/http';
 import { TrackServerService } from '../track-server.service';
-import { CLIENT_ID, API_KEY, SCOPES, DISCOVERY_DOC } from 'src/env';
+// import { CLIENT_ID, API_KEY, SCOPES, DISCOVERY_DOC } from 'src/env';
+import { GauthService } from '../gauth.service';
 
-// GOOGLE-BASED CODE
-declare const google: any;
-declare const gapi: any;
-// declare let tokenClient: any;
-// declare let gapiInited: boolean;
-// declare let gisInited:boolean;
-// GOOGLE-BASED CODE
+// // GOOGLE-BASED CODE
+// declare const google: any;
+// declare const gapi: any;
+// // GOOGLE-BASED CODE
 
 @Component({
   selector: 'app-login',
@@ -24,7 +22,8 @@ export class LoginComponent implements OnInit {
     private authService: SocialAuthService,
     private http: HttpClient,
     private trackService: TrackServerService,
-    private renderer: Renderer2) { }
+    private renderer: Renderer2,
+    public gauth: GauthService) { }
 
   user: SocialUser = new SocialUser;
   loggedIn = false;
@@ -41,95 +40,91 @@ export class LoginComponent implements OnInit {
       });
   }
 
-  // GOOGLE-BASED CODE
-  tokenClient: any;
-  gapiInited = false;
-  gisInited = false;
+  // // GOOGLE-BASED CODE
+  // tokenClient: any;
+  // gapiInited = false;
+  // gisInited = false;
 
-  gapiLoaded() {
-    gapi.load('client', this.initializeGapiClient);
-  }
+  // gapiLoaded() {
+  //   gapi.load('client', this.initializeGapiClient);
+  // }
 
-  async initializeGapiClient() {
-    console.log(API_KEY);
-    console.log(DISCOVERY_DOC);
-    await gapi.client.init({
-      apiKey: API_KEY,
-      // discoveryDocs: [this.DISCOVERY_DOC],
-      discoveryDocs: [DISCOVERY_DOC]
-    });
-    console.log(this.tokenClient);
-    console.log(this.gapiInited);
-    this.gapiInited = true;
-    // I DO NOT IMPLEMENT THIS - THE BUTTONS ARE ALWAYS VISIBLE
-    // maybeEnableButtons();
-  }
+  // async initializeGapiClient() {
+  //   await gapi.client.init({
+  //     apiKey: API_KEY,
+  //     // discoveryDocs: [this.DISCOVERY_DOC],
+  //     discoveryDocs: [DISCOVERY_DOC]
+  //   });
+  //   this.gapiInited = true;
+  //   // I DO NOT IMPLEMENT THIS - THE BUTTONS ARE ALWAYS VISIBLE
+  //   // maybeEnableButtons();
+  // }
 
-  gisLoaded() {
-    this.tokenClient = google.accounts.oauth2.initTokenClient({
-      client_id: CLIENT_ID,
-      scope: SCOPES,
-      callback: '', // defined later
-    });
-    this.gisInited = true;
-    // maybeEnableButtons();
-  }
+  // gisLoaded() {
+  //   this.tokenClient = google.accounts.oauth2.initTokenClient({
+  //     client_id: CLIENT_ID,
+  //     scope: SCOPES,
+  //     callback: '', // defined later
+  //   });
+  //   this.gisInited = true;
+  //   // maybeEnableButtons();
+  // }
 
-  handleAuthClick() {
-    this.tokenClient.callback = async (resp: any) => {
-      if (resp.error !== undefined) {
-        throw (resp);
-      }
-      // document.getElementById('signout_button').style.visibility = 'visible';
-      // document.getElementById('authorize_button').innerText = 'Refresh';
-      // await this.listFiles();
-    };
+  // handleAuthClick() {
+  //   this.tokenClient.callback = async (resp: any) => {
+  //     if (resp.error !== undefined) {
+  //       throw (resp);
+  //     }
+  //     // document.getElementById('signout_button').style.visibility = 'visible';
+  //     // document.getElementById('authorize_button').innerText = 'Refresh';
+  //     // await this.listFiles();
+  //   };
 
-    if (gapi.client.getToken() === null) {
-      // Prompt the user to select a Google Account and ask for consent to share their data
-      // when establishing a new session.
-      this.tokenClient.requestAccessToken({prompt: 'consent'});
-    } else {
-      // Skip display of account chooser and consent dialog for an existing session.
-      this.tokenClient.requestAccessToken({prompt: ''});
-    }
-  }
+  //   if (gapi.client.getToken() === null) {
+  //     // Prompt the user to select a Google Account and ask for consent to share their data
+  //     // when establishing a new session.
+  //     this.tokenClient.requestAccessToken({prompt: 'consent'});
+  //   } else {
+  //     // Skip display of account chooser and consent dialog for an existing session.
+  //     this.tokenClient.requestAccessToken({prompt: ''});
+  //   }
+  // }
 
-  handleSignoutClick() {
-    const token = gapi.client.getToken();
-    if (token !== null) {
-      google.accounts.oauth2.revoke(token.access_token);
-      gapi.client.setToken('');
-      // document.getElementById('content').innerText = '';
-      // document.getElementById('authorize_button').innerText = 'Authorize';
-      // document.getElementById('signout_button').style.visibility = 'hidden';
-    }
-  }
+  // handleSignoutClick() {
+  //   const token = gapi.client.getToken();
+  //   if (token !== null) {
+  //     google.accounts.oauth2.revoke(token.access_token);
+  //     gapi.client.setToken('');
+  //     // document.getElementById('content').innerText = '';
+  //     // document.getElementById('authorize_button').innerText = 'Authorize';
+  //     // document.getElementById('signout_button').style.visibility = 'hidden';
+  //   }
+  // }
 
-  async listFiles() {
-    let response;
-    try {
-      response = await gapi.client.drive.files.list({
-        'pageSize': 10,
-        'fields': 'files(id, name)',
-      });
-    } catch (err) {
-      console.log(err);
-      return;
-    }
-    const files = response.result.files;
-    if (!files || files.length == 0) {
-      // document.getElementById('content').innerText = 'No files found.';
-      console.log('No files found');
-      return;
-    }
-    // Flatten to string to display
-    const output = files.reduce(
-        (str: any, file: any) => `${str}${file.name} (${file.id})\n`,
-        'Files:\n');
-    console.log(output);
-  }
-  // GOOGLE-BASED CODE
+  // async listFiles() {
+  //   let response;
+  //   try {
+  //     response = await gapi.client.drive.files.list({
+  //       'pageSize': 10,
+  //       'fields': 'files(id, name)',
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //     return;
+  //   }
+  //   const files = response.result.files;
+  //   if (!files || files.length == 0) {
+  //     // document.getElementById('content').innerText = 'No files found.';
+  //     console.log('No files found');
+  //     return;
+  //   }
+  //   // Flatten to string to display
+  //   const output = files.reduce(
+  //       (str: any, file: any) => `${str}${file.name} (${file.id})\n`,
+  //       'Files:\n');
+  //   console.log(output);
+  // }
+  // // GOOGLE-BASED CODE
 
 
   // LOGIN COMPONENT IS SUBSCRIBED TO THE LIBRARY LOGIN SERVICE
@@ -146,23 +141,23 @@ export class LoginComponent implements OnInit {
 
     // GOOGLE-BASED CODE
     // TRY AND LOAD GOOGLE SCRIPTS INTO THE PAGE -> SUCCESS, THEY ARE THERE
-    const script1 = this.renderer.createElement('script');
-    this.renderer.setAttribute(script1, 'src', 'https://apis.google.com/js/api.js');
-    this.renderer.setAttribute(script1, 'async', '');
-    this.renderer.setAttribute(script1, 'defer', '');
-    this.renderer.listen(script1, 'load', (event) => {
-      this.gapiLoaded();
-    });
-    this.renderer.appendChild(document.body, script1);
+    // const script1 = this.renderer.createElement('script');
+    // this.renderer.setAttribute(script1, 'src', 'https://apis.google.com/js/api.js');
+    // this.renderer.setAttribute(script1, 'async', '');
+    // this.renderer.setAttribute(script1, 'defer', '');
+    // this.renderer.listen(script1, 'load', (event) => {
+    //   this.gapiLoaded();
+    // });
+    // this.renderer.appendChild(document.body, script1);
     
-    const script2 = this.renderer.createElement('script');
-    this.renderer.setAttribute(script2, 'src', 'https://accounts.google.com/gsi/client');
-    this.renderer.setAttribute(script2, 'async', '');
-    this.renderer.setAttribute(script2, 'defer', '');
-    this.renderer.listen(script2, 'load', (event) => {
-      this.gisLoaded();
-    });
-    this.renderer.appendChild(document.body, script2);
+    // const script2 = this.renderer.createElement('script');
+    // this.renderer.setAttribute(script2, 'src', 'https://accounts.google.com/gsi/client');
+    // this.renderer.setAttribute(script2, 'async', '');
+    // this.renderer.setAttribute(script2, 'defer', '');
+    // this.renderer.listen(script2, 'load', (event) => {
+    //   this.gisLoaded();
+    // });
+    // this.renderer.appendChild(document.body, script2);
     // GOOGLE-BASED CODE
   }
 }
